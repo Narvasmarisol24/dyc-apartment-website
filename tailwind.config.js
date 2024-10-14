823dac9c354f41987ea928355d3948f702e480c1
Marisol Narvas <ririmax21@gmail.com>
2024-10-15 03:08:16 +0800

    feat: navigation + home page hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./about/**/*.{js,html}"],
+  content: [
+    './index.html',
+    './about/**/*.{js,html}',
+    './lib/partials/**/*.{js,html}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'sans'],
+        serif: ['Libre Caslon Text', 'serif'],
+      },
+      colors: {
+        primary: '#b98632',
+        secondary: '#161b26',
+      },
+    },
   },
   plugins: [],
 };
