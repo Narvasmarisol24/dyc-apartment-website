45b8c91a2132598dc3b19173b104b638964e882f
Marisol Narvas <ririmax21@gmail.com>
2024-10-15 12:07:44 +0800

    chore: deploy worklow

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about/index.html"),
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+        units: resolve(__dirname, 'units/index.html'),
+        contact: resolve(__dirname, 'contact/index.html'),
       },
     },
   },
 
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "lib/partials"),
+      partialDirectory: resolve(__dirname, 'lib/partials'),
     }),
   ],
 });
